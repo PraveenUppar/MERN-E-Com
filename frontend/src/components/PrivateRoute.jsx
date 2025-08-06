2a2d797c75bcd55b7c19c34f3349783f071cd3f1
Praveen Uppar <praveenuppar718@gmail.com>
2025-08-06 14:36:55 +0530

    frontend proteted routes

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,7 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
+// useSelector: Redux hook that allows your component to read data from the Redux store.
+
 export default function PrivateRoute() {
+  // extracts the userInfo object from the user slice. This userInfo object contains data about the currently logged-in user,
   const { userInfo } = useSelector((state) => state.user);
+  // if the userInfo exisst then renders outletcomponent else redirects to login page
   return userInfo ? <Outlet /> : <Navigate to="/login" replace />;
 }
