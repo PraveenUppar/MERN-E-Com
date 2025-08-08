af50037c778f5a37fdb6d26089c27d3464c4372c
Praveen Uppar <praveenuppar718@gmail.com>
2025-08-08 18:35:17 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000",
+      "/api": "https://mern-e-com-w18b.onrender.com/",
     },
   },
 });
