f95095785fec551d821a33bd05a4d25830962381
Praveen Uppar <praveenuppar718@gmail.com>
2025-08-08 19:40:44 +0530

    project complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "https://mern-e-com-w18b.onrender.com/",
+      "/api": "http://localhost:5000",
     },
   },
 });
